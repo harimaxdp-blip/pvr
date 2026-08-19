4457b660ffce461882f8ff9de15a60b964db6b4b
harimaxdp-blip <harimaxdp@gmail.com>
2026-08-20 01:50:22 +0530

    update pvr project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MovieTicket from "./components/MovieTicket";
 
 function App() {
-  return <MovieTicket />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/ticket/:ticketId" element={<MovieTicket />} />
+        <Route path="/" element={<MovieTicket />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
