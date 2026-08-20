c124c0eff91792f731a9a4a64fe5fa17efb5bdee
harimaxdp-blip <harimaxdp@gmail.com>
2026-08-20 20:55:04 +0530

    update pvr project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MovieTicket from "./components/MovieTicket";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/ticket/:ticketId" element={<MovieTicket />} />
         <Route
-  path="/"
-  element={<Navigate to="/ticket/(iR8PHEN)y54KYkbuvJ6Ag==" replace />}
-/>
+          path="/ticket/:ticketId"
+          element={<MovieTicket />}
+        />
+
+        <Route
+          path="/"
+          element={
+            <Navigate
+              to="/ticket/(iR8PHEN)y54KYkbuvJ6Ag=="
+              replace
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
